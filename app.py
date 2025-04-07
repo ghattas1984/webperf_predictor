7500c8989349a3160b2e80ac17db42fee7b04d08
ghattas1984 <gattas.it.1984@gmail.com>
2025-04-07 17:32:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,19 @@ model = joblib.load("svm_model (3).pkl")
 st.set_page_config(page_title="Web Performance Predictor", page_icon="🌐")
 st.title("🌐 Website Performance Evaluation")
 
+st.markdown("""
+---
+
+📌 **Important Note**
+
+This application is part of a scientific research project that uses machine learning to evaluate website performance.  
+It extracts 10 real-world features from web pages without relying on any third-party APIs.  
+The model was trained on a balanced dataset and aims to provide accurate predictions to support web performance optimization.
+
+---
+""")
+
+
 url = st.text_input("Enter Website URL:")
 
 if st.button("Analyze") and url:
